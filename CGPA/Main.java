db5452f0dc2adcbf135ac9157091e35409bfdec8
Satyam Saxena <50856032+satyam-saxen@users.noreply.github.com>
2020-04-07 15:12:30 +0530

    Test code pushed to repository.

---
+++ CGPA/Main.java
@@ -0,0 +1,7 @@
+#include<iostream>
+int main()
+{
+  float m;
+  std::cin>>m;
+  std::cout<<m;
+}
---
+++ Its result time/Main.java
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+  int id,marks;
+  std::cin>>id;
+  std::cin>>marks;
+  std::cout<<"Id : "<<id;
+  std::cout<<"\nTotalmarks : "<<marks;
+  
+}
+    
